2c61292c10479dae7416da79329b2563a25b7bf9
bgautijonsson <bgautijonsson@gmail.com>
2024-11-09 12:15:57 +0000

    aðeins að taka út óþarfa kóða

--- R/predict_seats.R
+++ R/predict_seats.R
@@ -5,10 +5,6 @@ library(googlesheets4)
 library(purrr)
 library(scales)
 
-box::use(
-  R / election_utils[calculate_seats, seats_tibble, dhondt, jofnunarsaeti]
-)
-
 seats_tibble <- tribble(
   ~kjordaemi, ~n_seats, ~n_jofnun,
   "Reykjavík Suður", 9, 2,
@@ -20,18 +16,13 @@ seats_tibble <- tribble(
 )
 
 calculate_seats <- function(draw) {
-  box::use(
-    dplyr[arrange, pull],
-    tidyr[pivot_wider],
-    tibble[as_tibble, column_to_rownames]
-  )
 
   votes_matrix <- draw |>
     pivot_wider(names_from = kjordaemi, values_from = votes) |>
     column_to_rownames("flokkur") |>
     as.matrix()
 
-  available_seats <- seats_tibble() |>
+  available_seats <- seats_tibble |>
     arrange(kjordaemi) |>
     pull(n_seats)
 
@@ -60,24 +51,20 @@ calculate_seats <- function(draw) {
 
   kjordaemi_seats <- seats_matrix
 
-  available_jofnun <- seats_tibble() |>
+  available_jofnun <- seats_tibble |>
     arrange(kjordaemi) |>
     pull(n_jofnun)
 
   total_votes <- rowSums(votes_matrix)
   percent_votes <- total_votes / sum(total_votes)
-  percent_votes[percent_votes < 0.05] <- 0
 
   total_seats <- rowSums(seats_matrix)
-  seat_entitlement <- percent_votes * 63
 
   adjustment_seats <- numeric(9)
-  n_adjustment_seats <- numeric(9)
-  where_to_assign <- numeric(9)
+  n_adjustment_seats <- numeric(nrow(votes_matrix))
+  where_to_assign <- numeric(nrow(votes_matrix))
 
   for (i in seq_along(adjustment_seats)) {
-    seat_resid <- seat_entitlement - total_seats - n_adjustment_seats
-
     which_max <- which.max((total_votes * (percent_votes >= 0.05)) / (total_seats + n_adjustment_seats + 1))
 
     adjustment_seats[i] <- which_max
@@ -126,49 +113,21 @@ calculate_seats <- function(draw) {
     )
 }
 
-electorate_byagearea <- read_csv(here("data", "electorate_byagearea.csv")) |>
-  count(kjordaemi, wt = n_kjosendur, name = "n_kjosendur")
+# Breyta í RDS til að deila
+read_parquet(here("data", today(), "constituency_predictions.parquet")) |>
+  select(.draw, flokkur, kjordaemi, votes = value) |>
+  write_rds(here("data", today(), "constituency_predictions.rds"))
 
-gs4_auth(email = Sys.getenv("GOOGLE_MAIL"))
-
-kjordaemi <- read_sheet(
-  "https://docs.google.com/spreadsheets/d/1yEn5feIiltc4kWC61q57sGg_CpMCkxVFLIK9ySYxWQ4",
-  sheet = "kjordaemi"
-) |>
-  filter(Fyrirtæki == "Kosning") |>
-  select(-Ár, -Mánuður, -Dagur, -Fyrirtæki) |>
-  pivot_longer(
-    cols = -Flokkur,
-    names_to = "kjordaemi",
-    values_to = "p_kjordaemi"
-  ) |>
-  janitor::clean_names() |>
-  mutate(
-    p_kjordaemi = p_kjordaemi / sum(p_kjordaemi),
-    .by = flokkur
-  )
-
-draws <- read_parquet(here("data", today() - 1, "y_rep_draws.parquet")) |>
-  filter(
-    dags == max(dags)
-  ) |>
-  select(.draw, flokkur, value) |>
-  inner_join(
-    kjordaemi,
-    by = join_by(flokkur)
-  ) |>
-  inner_join(
-    electorate_byagearea,
-    by = join_by(kjordaemi)
-  ) |>
-  mutate(
-    value = value * p_kjordaemi * n_kjosendur
-  ) |>
-  summarise(
-    votes = sum(value),
-    .by = c(.draw, flokkur, kjordaemi)
-  ) |>
+draws <- read_parquet(here("data", today(), "constituency_predictions.parquet")) |>
+  select(.draw, flokkur, kjordaemi, votes = value) |>
   arrange(kjordaemi, flokkur)
+
+# Til að geta þæginlega labbað í gegnum fallið að ofan
+draw <- draws |>
+  filter(.draw == 1) |>
+  select(-.draw)
+
+
 
 d <- draws |>
   group_by(.draw) |>
@@ -207,50 +166,7 @@ ggsave(
   scale = 1.3
 )
 
-dir.create(here("data", today()))
+# dir.create(here("data", today()))
 
 d |>
   write_parquet(here("data", today(), "seats_draws.parquet"))
-
-
-
-d <- read_parquet(here("data", today() - 1, "y_rep_draws.parquet")) |>
-  filter(
-    dags == max(dags)
-  ) |>
-  select(.draw, flokkur, value) |>
-  inner_join(
-    kjordaemi,
-    by = join_by(flokkur)
-  )
-
-
-d |>
-  mutate(
-    kjor_fylgi = value * p_kjordaemi
-  ) |>
-  mutate(
-    kjor_fylgi = kjor_fylgi / sum(kjor_fylgi),
-    .by = c(.draw, kjordaemi)
-  ) |>
-  summarise(
-    mean = mean(kjor_fylgi),
-    lower = quantile(kjor_fylgi, 0.05),
-    upper = quantile(kjor_fylgi, 0.95),
-    .by = c(flokkur, kjordaemi)
-  ) |>
-  group_by(kjordaemi2 = kjordaemi) |>
-  arrange(desc(mean)) |>
-  group_map(
-    \(data, ...)  {
-      data |>
-        select(-kjordaemi) |>
-        gt() |>
-        fmt_percent(decimals = 0) |>
-        tab_header(
-          title = unique(data$kjordaemi)
-        ) |>
-        patchwork::wrap_table(space = "free")
-    }
-  ) |>
-  patchwork::wrap_plots()
